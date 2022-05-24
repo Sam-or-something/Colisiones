d621ce0e25b98854118c29976d8926f221597e2f
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-05-24 10:40:27 -0300

    Two Helix Game

---
+++ Assets/Scripts/HelixBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixBehavior : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(-4f, 2f, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(-4f, 0.5f, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "HELIX")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
